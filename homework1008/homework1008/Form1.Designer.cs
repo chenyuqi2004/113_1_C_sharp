cfd28ca60447c5f91caa52741e7f0e4f60fc61a9
chenyuqi2004 <a113221008@mail.shu.edu.tw>
2024-10-08 21:19:49 +0800

    homework

--- homework1008/homework1008/Form1.Designer.cs
+++ homework1008/homework1008/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -121,11 +122,22 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(86, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 43);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "點擊一張牌，顯示花色";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(543, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.BtnExit);
             this.Controls.Add(this.LblShow);
             this.Controls.Add(this.pictureBox5);
@@ -153,6 +165,7 @@
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.Label LblShow;
         private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Label label1;
     }
 }
 
